2ea43b127f6265c0d6e4d6a74a0b44afe09bc5bc
Dr Who Baker <tbaker.bhb@gmail.com>
2024-01-29 11:19:41 +0000

    Set up files and imported query selectors into JS

---
+++ Assets/script.js
@@ -0,0 +1,10 @@
+// -------------------------------//
+// Import HTML elements to target-//
+// -------------------------------//
+
+var currentDayEl = $("#currentDay");
+var containerEl = $("#container");
+
+// -------------------------------//
+// Set the time blocks to appear--//
+// -------------------------------//

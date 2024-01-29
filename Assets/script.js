09746e835f1dcf639dae8ff6bd33c8ac051e1c09
Dr Who Baker <tbaker.bhb@gmail.com>
2024-01-29 12:05:10 +0000

    added dayjs time

--- Assets/script.js
+++ Assets/script.js
@@ -6,5 +6,22 @@ var currentDayEl = $("#currentDay");
 var containerEl = $("#container");
 
 // -------------------------------//
+// Define any global variables----//
+// -------------------------------//
+
+var hours = [];
+// Let user set their working day to populate this variable
+
+// -------------------------------//
+// Set the day to appear using----//
+// DayJS--------------------------//
+// -------------------------------//
+
+// Note - the code below is utilised from FreeCodeCamps DayJs article
+const currentDate = dayjs();
+const formattedDate = currentDate.format("dddd, MMMM D");
+$(currentDayEl).text(formattedDate);
+
+// -------------------------------//
 // Set the time blocks to appear--//
 // -------------------------------//
